066e203e71730de02a3ac5342a36aaa3fad8ae7e
CTAS148 <reggifachri@lgsinarmas.com>
2025-05-08 04:58:05 +0700

    feat(common): dev exception & response

---
+++ src/main/java/com/pabrikx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pabrikx.common.exception;
+
+import com.pabrikx.common.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request){
+        ErrorResponse error = new ErrorResponse(
+          false,
+          ex.getMessage(),
+                LocalDateTime.now(),
+                request.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
